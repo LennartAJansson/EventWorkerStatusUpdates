dab15b3d00b464cc091b8420cf577751e833dccc
Lennart Jansson <lennart.jansson@sogeti.com>
2022-06-16 15:29:41 +0200

    Added dispose

--- WorkerService1/MyDataClass.cs
+++ WorkerService1/MyDataClass.cs
@@ -1,6 +1,6 @@
 ﻿namespace WorkerService1
 {
-    public class MyDataClass// : IDisposable
+    public class MyDataClass : IDisposable
     {
         public event MyEventType? MyStatusChanged;
         public int MyStatus { get => myStatus; set { myStatus = value; MyStatusChanged?.Invoke(myStatus); } }
@@ -9,11 +9,17 @@
         public event MyEventType? MyStatus2Changed;
         public int MyStatus2 { get => myStatus2; set { myStatus2 = value; MyStatus2Changed?.Invoke(myStatus2); } }
         private int myStatus2;
+        private readonly ILogger<MyDataClass> logger;
 
-        //public void Dispose()
-        //{
-        //    //throw new NotImplementedException();
-        //}
+        public MyDataClass(ILogger<MyDataClass> logger)
+        {
+            this.logger = logger;
+        }
+
+        public void Dispose()
+        {
+            logger?.LogWarning("Disposing MyDataClass");
+        }
     }
 
     public delegate void MyEventType(int status);
